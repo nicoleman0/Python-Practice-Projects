2a5656a00c537fae1073a7ca06049c52e2f1fb28
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-04-07 17:58:22 -0400

    Fix comments and improve code structure in data visualization script

--- Twitch-Data-Project/data.py
+++ Twitch-Data-Project/data.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Bar Graph: Featured Games
+# Bar Chart: Featured Games
 
 games = ["LoL", "Dota 2", "CS:GO", "DayZ", "HOS",
          "Isaac", "Shows", "Hearth", "WoT", "Agar.io"]
@@ -16,6 +16,9 @@ labels = ["US", "DE", "CA", "N/A", "GB", "TR",
 
 countries = [447, 66, 64, 49, 45, 28, 25, 20, 19, 17, 17, 279]
 
+colors = ['lightskyblue', 'firebrick', 'lightcoral', 'gainsboro', 'steelblue',
+          'lightpink', 'darkseagreen', 'sienna', 'indigo', 'gold', 'violet', 'forestgreen']
+
 # Line Graph: Time Series Analysis
 
 hour = range(24)
@@ -24,4 +27,38 @@ viewers_hour = [30, 17, 34, 29, 19, 14, 3, 2, 4, 9, 5,
                 48, 62, 58, 40, 51, 69, 55, 76, 81, 102, 120, 71, 63]
 
 
+# Bar Chart
+
 plt.bar(range(len(games)), viewers, color='mediumblue')
+
+plt.title('Featured Games - Viewers')
+plt.legend(['Twitch'])
+plt.xlabel('Games')
+plt.ylabel('Viewers')
+ax = plt.subplot()
+ax.set_xticks(range(len(games)))
+ax.set_xticklabels(games, rotation=30)
+
+plt.show()
+plt.clf()
+
+# Pie Chart
+
+explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+plt.pie(countries, explode=explode, colors=colors, shadow=True,
+        startangle=345, autopct='%1.0f%%', pctdistance=1.15)
+plt.title('League of Legends Viewers by Country')
+plt.legend(labels, loc='right')
+plt.axis('equal')
+plt.show()
+plt.clf()
+
+# Line Graph
+
+plt.plot(hour, viewers_hour)
+plt.title('Time Series')
+plt.xlabel('Hour')
+plt.ylabel('Viewers')
+plt.show()
+plt.clf()
